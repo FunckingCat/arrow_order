d963138afc082d060adc8b419e1ba0792461b032
FunckingCat <david99111@mail.ru>
2019-10-03 18:05:54 +0300

    class API development started

--- ArrowOrderApp/script/classes.js
+++ ArrowOrderApp/script/classes.js
@@ -1,1 +1,66 @@
+class User {
+  constructor(name, contact){
+    this.name = name;
+    this.contact = contact;
+  }
 
+  set name(value){
+    this._name = value
+  }
+
+  get name(){
+    return this._name
+  }
+
+  set contact(value){
+    if (validatePhone(value)){
+      this._phone = validatePhone(value)
+    }else if (validateInsta(value)){
+      this._insta = validateInsta(value)
+    }else{
+      throw new Error('notValidContact')
+    }
+  }
+
+  get contact(){
+    if (this._insta){
+      return ['insta',this._insta]
+    }else if(this._phone){
+      return ['phone', this._phone]
+    }else{
+      throw new Error('noContacts')
+    }
+  }
+}
+
+class Order{
+  constructor(){
+
+  }
+}
+
+class Cake extends Order{
+  constructor(){
+    this.inner = inner;
+    this.biskit = biskit;
+    this.cream = cream;
+  }
+}
+
+class Bickit extends Cake{
+  constructor(){
+
+  }
+}
+
+class Honey extends Cake{
+  constructor(){
+
+  }
+}
+
+class Digit extends Honey{
+  constructor(){
+
+  }
+}
--- ArrowOrderApp/script/util.js
+++ ArrowOrderApp/script/util.js
@@ -1,16 +1,17 @@
 function validatePhone(str) {
     reg = /\d/gi
     match = str.match(reg)
-    if (match.length == 11 && (match[0] == 7 || match[0] == 8) && match[1] == 9){
-        return true
+    if (match != null && match.length == 11 && (match[0] == 7 || match[0] == 8) && match[1] == 9){
+        return match.join('')
     }
     return false
 }
 
 function validateInsta(str) {
     reg = /^@*[\w\d\.]+$/ig
-    if (str.match(reg)){
-        return true
+    match = str.match(reg)
+    if (match){
+        return match[0]
     }
     return false
 }
