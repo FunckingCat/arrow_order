f54e2362e2126cbd52026212c3ef6a127963174a
FunckingCat <david99111@mail.ru>
2019-10-07 19:22:33 +0300

    Login page in progress

--- ArrowOrderApp/script/script.js
+++ ArrowOrderApp/script/script.js
@@ -1,4 +1,4 @@
-$('.login form input[type="button"]').on('click', function() {
-  name = $('.login form div input')[0];
-  console.log(name)
-});
+$('.login form input[type="button"]').on('click', loginValidationLabel);
+//Обработчик правильности ввода; Выводит надписи
+$('.login form input[type="button"]').on('click', loginIfValidInput);
+//Перекидывает на главную страницу если данные корректны
--- ArrowOrderApp/script/util.js
+++ ArrowOrderApp/script/util.js
@@ -16,6 +16,49 @@ function validateInsta(str) {
     return false
 }
 
+function loginValidationLabel() {
+  $('.login form div input').nextAll('.valid').html('').end().each(function() {
+    if ($(this).val() != ''){
+      if($(this).attr('validate')){
+        if(validatePhone($(this).val())){
+          $(this).nextAll('.valid').html('Корректный номер').css('color','green');
+        }else if(validateInsta($(this).val())){
+            $(this).nextAll('.valid').html('Корректный инстаграм').css('color','green');
+        }else{
+          $(this).nextAll('.valid').html('Некорректный ввод').css('color','rgb(134, 0, 0)');
+        }
+      }
+    }else{
+      $(this).nextAll('.valid').html('Обязательное поле').css('color','rgb(134, 0, 0)');
+    }
+  });
+}
+
+function loginIfValidInput() {
+  result = $('.login form div input').map(function() {
+    val = $(this).val()
+    if (val == '') return false
+    if ($(this).attr('validate')){
+      if (validateInsta(val)||validatePhone(val)){
+          return true
+      }else{
+        return false
+      }
+    }
+    return true
+  }).get();
+  if (result.indexOf(false) == -1){
+    //Сохранение данных в LocalStorage
+    vals = $('.login form div input').map(function() {
+      return $(this).val()
+    }).get();
+    user = new User(vals[0], vals[1]);
+    console.log(user.contact)
+    console.log('Переход на другую страницу')
+    //Дописать код перехода на главную страницу или скрытия окна решистрации
+  }
+}
+
 function formulaParser(formula) {//Должна вернуть объект скласса торта
   let reg = /^(?<type>[A-Z]+):(?<filling>[A-Z]+)-(?<sponge>[A-Z]+)~(?<cream>[A-Z]+)/i
   let groups = formula.match(reg).groups;
