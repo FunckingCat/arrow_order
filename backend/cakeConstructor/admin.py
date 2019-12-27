294f91ab0e7686b4518ab0bc7535f8d88145ac1d
David <david99111@mail.ru>
2019-12-27 17:50:32 +0300

    Biscits Fillings Creams models added

--- backend/cakeConstructor/admin.py
+++ backend/cakeConstructor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import  Biscuit, Filling, Cream
+admin.site.register(Biscuit)
+admin.site.register(Filling)
+admin.site.register(Cream)
--- backend/cakeConstructor/models.py
+++ backend/cakeConstructor/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Biscuit (models.Model):
+
+    name = models.CharField(max_length = 60, verbose_name = 'Название')
+
+    def __str__ (self):
+        return self.name
+
+class Filling (models.Model):
+
+    name = models.CharField(max_length = 60, verbose_name = 'Название')
+
+    def __str__ (self):
+        return self.name
+
+class Cream (models.Model):
+
+    name = models.CharField(max_length = 60, verbose_name = 'Название')
+
+    def __str__ (self):
+        return self.name
