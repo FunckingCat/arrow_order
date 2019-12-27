843a2d8449f67d67cd43441f18d6f3923466c332
David <david99111@mail.ru>
2019-12-27 19:09:35 +0300

    ManyToMany relations added in constructor models

--- backend/cakeConstructor/models.py
+++ backend/cakeConstructor/models.py
@@ -2,6 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+class Filling (models.Model):
+
+    name = models.CharField(max_length = 60, verbose_name = 'Название')
+    avalibleBiscuits = models.ManyToManyField('Biscuit', blank=True, related_name='avalibleFillings', verbose_name = 'Доступные бисквиты')
+    avalibleCreams = models.ManyToManyField('Cream', blank=True, related_name='avalibleFillings', verbose_name = 'Доступные кремы')
+
+    def __str__ (self):
+        return self.name
+
+    def all(self):
+        creams = []
+        for cream in self.avalibleCreams.all():
+            creams.append(cream.name)
+        biscuits = []
+        for biscuit in self.avalibleBiscuits.all():
+            biscuits.append(biscuit.name)
+        return {
+            'name' : self.name,
+            'biscuits' : biscuits,
+            'creams' : creams,
+        }
+
 class Biscuit (models.Model):
 
     name = models.CharField(max_length = 60, verbose_name = 'Название')
@@ -9,12 +31,11 @@ class Biscuit (models.Model):
     def __str__ (self):
         return self.name
 
-class Filling (models.Model):
+    def all(self):
+        return {
+            'name' : self.name,
+        }
 
-    name = models.CharField(max_length = 60, verbose_name = 'Название')
-
-    def __str__ (self):
-        return self.name
 
 class Cream (models.Model):
 
@@ -22,3 +43,8 @@ class Cream (models.Model):
 
     def __str__ (self):
         return self.name
+
+    def all(self):
+        return {
+            'name' : self.name,
+        }
--- backend/cakeConstructor/views.py
+++ backend/cakeConstructor/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Filling, Biscuit, Cream
 # Create your views here.
 
 def main (request):
-    return JsonResponse({
-        '1' : 'Works',
-    })
+    response = { 'values' : []}
+    for item in Filling.objects.all():
+        response['values'].append(item.all())
+    return JsonResponse(response)
