75082dd48f30bc524a52c9eba8f0615f6a67c492
David Zh <david99111@mail.ru>
2020-01-22 14:41:40 +0300

    paragraph max-height bug fix

--- arrow-order-react/src/reducers/domenReducer.js
+++ arrow-order-react/src/reducers/domenReducer.js
@@ -1,5 +1,6 @@
-//const initialState = 'http://localhost:8000'
-const initialState = 'http://192.168.1.66:8000'
+const initialState = 'http://localhost:8000'
+//const initialState = 'http://192.168.1.66:8000'
+//const initialState = 'http://192.168.1.73:8000'
 
 const domenReducer = (state = initialState) => {
     return state
