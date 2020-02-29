57d6d493abac7d7ea3c63e5e7f033f768b006d90
David <david99111@mail.ru>
2020-02-29 15:40:19 +0300

    Remover colors defining function in backend

--- backend/constructor/urls.py
+++ backend/constructor/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.allInfo),
-    path('getColors/', views.getColors),
-    path('getColors/<str:parts>/', views.getColors),
     path('<str:con_type>/<str:ingredient>/', views.handaleRequest),
     path('<str:con_type>/<str:ingredient>/<str:params>/', views.handaleRequest),
 ]
