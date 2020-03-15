f68a449f888153ec16b2666d272b0ef6be860df6
David <david99111@mail.ru>
2020-03-15 09:31:10 +0300

    Created Logger MOdels

--- backend/logger/admin.py
+++ backend/logger/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Bug, New_user, Error
 # Register your models here.
+
+admin.site.register(Bug)
+admin.site.register(New_user)
+admin.site.register(Error)
--- backend/logger/models.py
+++ backend/logger/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Log_abc(models.Model):
+
+    class Meta:
+        abstract = True
+        ordering = ['date']
+        verbose_name = 'Отчет'
+        verbose_name_plural = 'Отчеты'
+
+    date = models.DateTimeField(auto_now_add = True)
+
+class Bug(Log_abc):
+
+    class Meta:
+        verbose_name = 'Баг'
+        verbose_name_plural = 'Баги'
+
+    state = models.TextField()
+    reason = models.CharField(max_length = 150)
+    descr = models.TextField()
+
+class New_user(Log_abc):
+
+    class Meta:
+        verbose_name = 'Новый пользователь'
+        verbose_name_plural = 'Новые пользователи'
+
+    name = models.CharField(max_length = 100)
+    contact = models.CharField(max_length = 100)
+    navigator =  models.CharField(max_length = 100)
+    clientWidth = models.IntegerField()
+    clientHeight = models.IntegerField()
+
+class Error(Log_abc):
+
+    class Meta:
+        verbose_name = 'Ошибка'
+        verbose_name_plural = 'Ошибки'
+
+    name = models.CharField(max_length = 100)
+    contact = models.CharField(max_length = 100)
+    descr = models.TextField()
+
+
+    
