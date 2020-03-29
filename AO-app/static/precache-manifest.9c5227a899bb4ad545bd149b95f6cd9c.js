a6eb50e9a2a7bb56caa8739e9e721597f292136e
David <david99111@mail.ru>
2020-03-29 14:07:55 +0300

    First production build integrated

---
+++ AO-app/static/precache-manifest.9c5227a899bb4ad545bd149b95f6cd9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7d8700d46e157cdca632604d60555d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f639278c83b7f8af4c2",
+    "url": "/static/css/main.446b7551.chunk.css"
+  },
+  {
+    "revision": "eea00d37ef9f86b6c778",
+    "url": "/static/js/2.65ef831d.chunk.js"
+  },
+  {
+    "revision": "5f639278c83b7f8af4c2",
+    "url": "/static/js/main.b1a9a86d.chunk.js"
+  },
+  {
+    "revision": "9169922b6069d214dfa2",
+    "url": "/static/js/runtime-main.a69a13dd.js"
+  }
+]);
